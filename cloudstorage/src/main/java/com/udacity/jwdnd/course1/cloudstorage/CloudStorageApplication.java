8b373d9e2d1d17a8fa03e1f573bfea0a7826e334
Sigve <sigvetl@student.matnat.uio.no>
2020-12-15 17:36:07 +0100

    Added support for larger files up to 100 mb

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -2,6 +2,11 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.MultipartFilter;
 
 @SpringBootApplication
 public class CloudStorageApplication {
@@ -10,4 +15,19 @@ public class CloudStorageApplication {
 		SpringApplication.run(CloudStorageApplication.class, args);
 	}
 
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver commonsMultipartResolver=new CommonsMultipartResolver();
+		commonsMultipartResolver.setDefaultEncoding("utf-8");
+		commonsMultipartResolver.setMaxUploadSize(105242880);
+		return  commonsMultipartResolver;
+	}
+	@Bean
+	public FilterRegistrationBean multipartFilterRegistrationBean() {
+		final MultipartFilter multipartFilter = new MultipartFilter();
+		final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(multipartFilter);
+		filterRegistrationBean.addInitParameter("multipartResolverBeanName", "commonsMultipartResolver");
+		return filterRegistrationBean;
+	}
 }
+
